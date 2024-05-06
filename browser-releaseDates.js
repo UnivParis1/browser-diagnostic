65133a6d92256a00f43c68b5ee50d12c03b326eb
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2024-05-06 21:33:07 +0200

    ./get-browser-releaseDates.sh

---
+++ browser-releaseDates.js
@@ -0,0 +1,264 @@
+browser_releaseDates = {
+firefox: 
+[{"cycle":"125","releaseDate":"2024-04-16","eol":false,"latest":"125.0.3","latestReleaseDate":"2024-04-29","lts":false},{"cycle":"124","releaseDate":"2024-03-19","eol":"2024-04-16","latest":"124.0.2","latestReleaseDate":"2024-04-02","lts":false},{"cycle":"123","releaseDate":"2024-02-20","eol":"2024-03-19","latest":"123.0.1","latestReleaseDate":"2024-03-05","lts":false},{"cycle":"122","releaseDate":"2024-01-23","eol":"2024-02-20","latest":"122.0.1","latestReleaseDate":"2024-02-06","lts":false},{"cycle":"121","releaseDate":"2023-12-19","eol":"2024-01-23","latest":"121.0.1","latestReleaseDate":"2024-01-09","lts":false},{"cycle":"120","releaseDate":"2023-11-21","eol":"2023-12-19","latest":"120.0.1","latestReleaseDate":"2023-11-30","lts":false},{"cycle":"119","releaseDate":"2023-10-24","eol":"2023-11-21","latest":"119.0.1","latestReleaseDate":"2023-11-07","lts":false},{"cycle":"118","releaseDate":"2023-09-26","eol":"2023-10-24","latest":"118.0.2","latestReleaseDate":"2023-10-10","lts":false},{"cycle":"117","releaseDate":"2023-08-29","eol":"2023-09-26","latest":"117.0.1","latestReleaseDate":"2023-09-12","lts":false},{"cycle":"116","releaseDate":"2023-08-01","eol":"2023-08-29","latest":"116.0.3","latestReleaseDate":"2023-08-16","lts":false},{"cycle":"115","lts":true,"releaseDate":"2023-07-04","eol":"2024-10-01","latest":"115.10.0","latestReleaseDate":"2024-04-16"},{"cycle":"114","releaseDate":"2023-06-06","eol":"2023-07-04","latest":"114.0.2","latestReleaseDate":"2023-06-20","lts":false},{"cycle":"113","releaseDate":"2023-05-09","eol":"2023-06-06","latest":"113.0.2","latestReleaseDate":"2023-05-23","lts":false},{"cycle":"112","releaseDate":"2023-04-11","eol":"2023-05-09","latest":"112.0.2","latestReleaseDate":"2023-04-25","lts":false},{"cycle":"111","releaseDate":"2023-03-14","eol":"2023-04-11","latest":"111.0.1","latestReleaseDate":"2023-03-21","lts":false},{"cycle":"110","releaseDate":"2023-02-14","eol":"2023-03-14","latest":"110.0.1","latestReleaseDate":"2023-02-28","lts":false},{"cycle":"109","releaseDate":"2023-01-17","eol":"2023-02-14","latest":"109.0.1","latestReleaseDate":"2023-01-31","lts":false},{"cycle":"108","releaseDate":"2022-12-13","eol":"2023-01-17","latest":"108.0.2","latestReleaseDate":"2023-01-05","lts":false},{"cycle":"107","releaseDate":"2022-11-15","eol":"2022-12-13","latest":"107.0.1","latestReleaseDate":"2022-11-29","lts":false},{"cycle":"106","releaseDate":"2022-10-18","eol":"2022-11-15","latest":"106.0.5","latestReleaseDate":"2022-11-04","lts":false},{"cycle":"105","releaseDate":"2022-09-20","eol":"2022-10-18","latest":"105.0.3","latestReleaseDate":"2022-10-07","lts":false},{"cycle":"104","releaseDate":"2022-08-23","eol":"2022-09-20","latest":"104.0.2","latestReleaseDate":"2022-09-06","lts":false},{"cycle":"103","releaseDate":"2022-07-26","eol":"2022-08-23","latest":"103.0.2","latestReleaseDate":"2022-08-09","lts":false},{"cycle":"102","lts":true,"releaseDate":"2022-06-28","eol":"2023-09-26","latest":"102.15.1","latestReleaseDate":"2023-09-12"},{"cycle":"91","lts":true,"releaseDate":"2021-08-10","eol":"2022-09-20","latest":"91.13.0","latestReleaseDate":"2022-08-23"},{"cycle":"78","lts":true,"releaseDate":"2020-06-30","eol":"2021-11-02","latest":"78.15.0","latestReleaseDate":"2021-10-05"}]
+,
+chrome: 
+[
+  {
+    "version": "124.0.6367.93",
+    "milestone": 124,
+    "date": "2024-4-30"
+  },
+  {
+    "version": "124.0.6367.118",
+    "milestone": 124,
+    "date": "2024-4-30"
+  },
+  {
+    "version": "124.0.6367.92",
+    "milestone": 124,
+    "date": "2024-4-30"
+  },
+  {
+    "version": "124.0.6367.91",
+    "milestone": 124,
+    "date": "2024-4-25"
+  },
+  {
+    "version": "124.0.6367.63",
+    "milestone": 124,
+    "date": "2024-4-25"
+  },
+  {
+    "version": "124.0.6367.78",
+    "milestone": 124,
+    "date": "2024-4-23"
+  },
+  {
+    "version": "124.0.6367.62",
+    "milestone": 124,
+    "date": "2024-4-23"
+  },
+  {
+    "version": "124.0.6367.61",
+    "milestone": 124,
+    "date": "2024-4-22"
+  },
+  {
+    "version": "123.0.6312.123",
+    "milestone": 123,
+    "date": "2024-4-15"
+  },
+  {
+    "version": "124.0.6367.60",
+    "milestone": 124,
+    "date": "2024-4-15"
+  },
+  {
+    "version": "123.0.6312.124",
+    "milestone": 123,
+    "date": "2024-4-15"
+  },
+  {
+    "version": "123.0.6312.122",
+    "milestone": 123,
+    "date": "2024-4-10"
+  },
+  {
+    "version": "123.0.6312.107",
+    "milestone": 123,
+    "date": "2024-4-10"
+  },
+  {
+    "version": "123.0.6312.106",
+    "milestone": 123,
+    "date": "2024-4-4"
+  },
+  {
+    "version": "123.0.6312.88",
+    "milestone": 123,
+    "date": "2024-4-2"
+  },
+  {
+    "version": "123.0.6312.105",
+    "milestone": 123,
+    "date": "2024-4-2"
+  },
+  {
+    "version": "123.0.6312.86",
+    "milestone": 123,
+    "date": "2024-3-26"
+  },
+  {
+    "version": "123.0.6312.60",
+    "milestone": 123,
+    "date": "2024-3-26"
+  },
+  {
+    "version": "123.0.6312.59",
+    "milestone": 123,
+    "date": "2024-3-25"
+  },
+  {
+    "version": "123.0.6312.58",
+    "milestone": 123,
+    "date": "2024-3-19"
+  },
+  {
+    "version": "122.0.6261.131",
+    "milestone": 122,
+    "date": "2024-3-19"
+  },
+  {
+    "version": "122.0.6261.129",
+    "milestone": 122,
+    "date": "2024-3-14"
+  },
+  {
+    "version": "123.0.6312.46",
+    "milestone": 123,
+    "date": "2024-3-13"
+  },
+  {
+    "version": "122.0.6261.130",
+    "milestone": 122,
+    "date": "2024-3-13"
+  },
+  {
+    "version": "122.0.6261.128",
+    "milestone": 122,
+    "date": "2024-3-12"
+  },
+  {
+    "version": "122.0.6261.113",
+    "milestone": 122,
+    "date": "2024-3-12"
+  },
+  {
+    "version": "122.0.6261.112",
+    "milestone": 122,
+    "date": "2024-3-7"
+  },
+  {
+    "version": "122.0.6261.96",
+    "milestone": 122,
+    "date": "2024-3-5"
+  },
+  {
+    "version": "122.0.6261.111",
+    "milestone": 122,
+    "date": "2024-3-5"
+  },
+  {
+    "version": "122.0.6261.95",
+    "milestone": 122,
+    "date": "2024-2-29"
+  },
+  {
+    "version": "122.0.6261.94",
+    "milestone": 122,
+    "date": "2024-2-27"
+  },
+  {
+    "version": "122.0.6261.71",
+    "milestone": 122,
+    "date": "2024-2-27"
+  },
+  {
+    "version": "122.0.6261.70",
+    "milestone": 122,
+    "date": "2024-2-26"
+  },
+  {
+    "version": "122.0.6261.69",
+    "milestone": 122,
+    "date": "2024-2-22"
+  },
+  {
+    "version": "121.0.6167.189",
+    "milestone": 121,
+    "date": "2024-2-22"
+  },
+  {
+    "version": "122.0.6261.57",
+    "milestone": 122,
+    "date": "2024-2-20"
+  },
+  {
+    "version": "121.0.6167.187",
+    "milestone": 121,
+    "date": "2024-2-20"
+  },
+  {
+    "version": "121.0.6167.185",
+    "milestone": 121,
+    "date": "2024-2-15"
+  },
+  {
+    "version": "122.0.6261.39",
+    "milestone": 122,
+    "date": "2024-2-14"
+  },
+  {
+    "version": "121.0.6167.186",
+    "milestone": 121,
+    "date": "2024-2-14"
+  },
+  {
+    "version": "121.0.6167.184",
+    "milestone": 121,
+    "date": "2024-2-13"
+  },
+  {
+    "version": "121.0.6167.162",
+    "milestone": 121,
+    "date": "2024-2-13"
+  },
+  {
+    "version": "121.0.6167.161",
+    "milestone": 121,
+    "date": "2024-2-8"
+  },
+  {
+    "version": "121.0.6167.160",
+    "milestone": 121,
+    "date": "2024-2-6"
+  },
+  {
+    "version": "121.0.6167.141",
+    "milestone": 121,
+    "date": "2024-2-6"
+  },
+  {
+    "version": "121.0.6167.140",
+    "milestone": 121,
+    "date": "2024-2-1"
+  },
+  {
+    "version": "121.0.6167.87",
+    "milestone": 121,
+    "date": "2024-1-30"
+  },
+  {
+    "version": "121.0.6167.139",
+    "milestone": 121,
+    "date": "2024-1-30"
+  },
+  {
+    "version": "121.0.6167.86",
+    "milestone": 121,
+    "date": "2024-1-29"
+  },
+  {
+    "version": "121.0.6167.85",
+    "milestone": 121,
+    "date": "2024-1-22"
+  }
+],
+safari: 
+{"14.0.1":"12 Nov 2020","16.6.1":"21 Sep 2023","16.5.2":"10 Jul 2023","14.1.2":"19 Jul 2021","13.1.2":"15 Jul 2020","16.6":"24 Jul 2023","14.0":"16 Sep 2020","17":"26 Sep 2023","15.5":"16 May 2022","16.4":"27 Mar 2023","16.2":"13 Dec 2022","17.1":"25 Oct 2023","17.3":"22 Jan 2024","17.3.1":"09 Feb 2024","15":"20 Sep 2021","16.4.1":"07 Apr 2023","15.1":"27 Oct 2021","15.3":"26 Jan 2022","17.1.2":"30 Nov 2023","13.0.5":"28 Jan 2020","15.6.1":"18 Aug 2022","15.6":"20 Jul 2022","15.4":"15 Mar 2022","15.2":"14 Dec 2021","14.0.3":"01 Feb 2021","17.2.1":"19 Dec 2023","16.5":"18 May 2023","13.1.1":"26 May 2020","14.1.1":"24 May 2021","17.4.1":"25 Mar 2024","13.1":"24 Mar 2020","17.4":"07 Mar 2024","14.1":"26 Apr 2021","16.5.1":"21 Jun 2023","17.2":"11 Dec 2023","16.1":"24 Oct 2022","16.3":"23 Jan 2023","14.0.2":"14 Dec 2020","16":"12 Sep 2022"}
+,
+edge: 
+[{"isExtended":null,"date":"Stable Channel","milestone":"Version"},{"isExtended":null,"date":"16-May-2024","milestone":"125"},{"milestone":"124","date":"18-Apr-2024","isExtended":1},{"date":"22-Mar-2024","isExtended":null,"milestone":"123"},{"milestone":"122","date":"23-Feb-2024","isExtended":1},{"isExtended":null,"date":"25-Jan-2024","milestone":"121"},{"milestone":"120","isExtended":1,"date":"07-Dec-2023"},{"isExtended":null,"date":"02-Nov-2023","milestone":"119"},{"milestone":"118","isExtended":1,"date":"13-Oct-2023"},{"isExtended":null,"date":"15-Sep-2023","milestone":"117"},{"date":"21-Aug-2023","isExtended":1,"milestone":"116"},{"milestone":"115","isExtended":null,"date":"21-Jul-2023"},{"isExtended":1,"date":"02-Jun-2023","milestone":"114"},{"milestone":"113","date":"05-May-2023","isExtended":null},{"milestone":"112","isExtended":1,"date":"06-Apr-2023"},{"milestone":"111","isExtended":null,"date":"13-Mar-2023"},{"milestone":"110","isExtended":1,"date":"09-Feb-2023"},{"date":"12-Jan-2023","isExtended":null,"milestone":"109"},{"date":"05-Dec-2022","isExtended":1,"milestone":"108"},{"milestone":"107","date":"27-Oct-2022","isExtended":null},{"isExtended":1,"date":"03-Oct-2022","milestone":"106"},{"milestone":"105","isExtended":null,"date":"01-Sep-2022"},{"isExtended":1,"date":"05-Aug-2022","milestone":"104"},{"isExtended":null,"date":"23-Jun-2022","milestone":"103"},{"milestone":"102","isExtended":1,"date":"31-May-2022"},{"milestone":"101","isExtended":null,"date":"28-Apr-2022"},{"isExtended":1,"date":"01-Apr-2022","milestone":"100"},{"milestone":"99","isExtended":null,"date":"03-Mar-2022"},{"milestone":"98","isExtended":1,"date":"03-Feb-2022"},{"date":"06-Jan-2022","isExtended":null,"milestone":"97"},{"milestone":"96","date":"19-Nov-2021","isExtended":1},{"milestone":"95","date":"21-Oct-2021","isExtended":null},{"date":"24-Sep-2021","isExtended":1,"milestone":"94"},{"milestone":"93","date":"02-Sep-2021","isExtended":null},{"isExtended":null,"date":"22-July-2021","milestone":"92"},{"milestone":"91","date":"27-May-2021","isExtended":null},{"milestone":"90","isExtended":null,"date":"15-Apr-2021"},{"milestone":"89","isExtended":null,"date":"08-Mar-2021"},{"isExtended":null,"date":"21-Jan-2021","milestone":"88"}]
+,
+}
