39b291a5540d0c0499d395d73abe2eabb2142cf1
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2024-05-30 10:33:52 +0200

    get new samsungbrowser versions while keeping previous one

--- get-browser-releaseDates.sh
+++ get-browser-releaseDates.sh
@@ -6,7 +6,8 @@ curl -s https://endoflife.date/api/firefox.json | jq . > firefox-versions.json
 
 curl -s https://support.apple.com/en-us/HT201222 | perl -MJSON -lne 'if (/Safari ([\d.]*)/) { $v = $1; $td = 1 } elsif ($td && /<td.*?>(.*?)</) { $td++; if ($td == 3) { $h{$v} = $1; $td = 0 } } END { print encode_json(\%h) }' | jq --sort-keys > safari-versions.json
 
-curl -s https://apkpure.fr/fr/samsung-internet-browser/com.sec.android.app.sbrowser/versions | perl -MJSON -lne 'if (/data-dt-version="([^"]*)"/) { $v = $1 } elsif (m!"update-on">([^<]*)</span>! && $v) { $h{$v} = $1; $v = "" } END { print encode_json(\%h) }' | jq --sort-keys > samsungbrowser-versions.json
+(perl -lne '$t = /^samsungbrowser:/ ... /\}/; print if $t > 1' browser-releaseDates.js ;
+ curl -s https://apkpure.fr/fr/samsung-internet-browser/com.sec.android.app.sbrowser/versions | perl -MJSON -lne 'if (/data-dt-version="([^"]*)"/) { $v = $1 } elsif (m!"update-on">([^<]*)</span>! && $v) { $h{$v} = $1; $v = "" } END { print encode_json(\%h) }') | jq -r -s --sort-keys '.[0] + .[1]' > samsungbrowser-versions.json
 
 curl -s https://raw.githubusercontent.com/MicrosoftDocs/Edge-Enterprise/public/edgeenterprise/microsoft-edge-release-schedule.md | perl -MJSON -lne 'if (/[|]\s*Release/) { @one = map { s/<.*//; $_ } split /\s*[|]\s*/; $one[4] =~ s/Week of //; push @l, { milestone => $one[1], date => $one[4], $one[5] =~ /-.*-\d\d\d\d/ ? (isExtended => JSON::true) : () } } END { print encode_json(\@l) }' | jq --sort-keys > edge-versions.json
 
